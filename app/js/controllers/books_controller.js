dab3b65f25bdb804316d0aae55bda13401bf0517
callum.cooper <Callum.Cooper@digital.hmrc.gov.uk>
2016-07-25 17:06:01 +0100

    Add Buttons to pages so that navigation can be easily accessed.

--- app/js/controllers/books_controller.js
+++ app/js/controllers/books_controller.js
@@ -1,11 +1,27 @@
 // with $resource
-angular.module("app").controller("BooksResourceController", function ($scope, BookResource) {
+angular.module("app").controller("BooksResourceController", function ($scope, $location, BookResource, AuthenticationService) {
   // because the stubbed endpoint returns an array of results, .query() is used
   // if the endpoint returned an object, you would use .get()
   $scope.books = BookResource.query();
+
+  // create logout success callback
+  var onLogoutSuccess = function(response) {
+    $location.path('/login');
+  };
+
+  // Redirect to authentication service via a logout success callback
+  $scope.logout = function() {
+    AuthenticationService.logout().success(onLogoutSuccess);
+  };
+
+  // Redirect to home page
+  $scope.home = function() {
+    $location.path('/home');
+  };
 });
 
 // with $http
 angular.module("app").controller("BooksHttpController", function ($scope, books) {
   $scope.books = books;
 });
+
--- app/js/controllers/home_controller.js
+++ app/js/controllers/home_controller.js
@@ -2,11 +2,18 @@ angular.module("app").controller('HomeController', function($scope, $location, A
   $scope.title = "Angular.JS";
   $scope.message = "Mouse Over these images to see a directive at work";
 
+  // create logout success callback
   var onLogoutSuccess = function(response) {
     $location.path('/login');
   };
 
+  // Redirect to authentication service via a logout success callback
   $scope.logout = function() {
     AuthenticationService.logout().success(onLogoutSuccess);
+  };
+
+  // Redirect to books page
+  $scope.books = function() {
+  	$location.path('/$resource/list-of-books');
   };
 });
